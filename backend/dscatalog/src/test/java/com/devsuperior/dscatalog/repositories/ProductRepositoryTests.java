405a99a7041cee73469d457266b86cd189d318c0
Carlos Henrique <ichfs9108@gmail.com>
2023-09-08 00:42:23 -0300

    Primeiro Teste de repository

---
+++ backend/dscatalog/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscatalog.repositories;
+
+import com.devsuperior.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+
+        Long productId = 1L;
+
+        repository.deleteById(productId);
+
+        Optional<Product> result = repository.findById(1L);
+
+        Assertions.assertFalse(result.isPresent());
+    }
+
+
+}
