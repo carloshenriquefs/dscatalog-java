74663867b9daf58cc9648c4618dd53d6b214ac0a
Carlos Henrique <ichfs9108@gmail.com>
2023-09-09 01:57:20 -0300

    Implementando teste findAllPaged

--- backend/dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,15 +1,28 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.tests.Factory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -27,12 +40,16 @@ public class ProductServiceTests {
     private Long existingId;
     private Long nonExistingId;
     private Long dependentId;
+    private PageImpl<Product> page;
+    private Product product;
 
     @BeforeEach
     void setup() throws Exception {
         existingId = 1L;
         nonExistingId = 1000L;
         dependentId = 100L;
+        product = Factory.createProduct();
+        page = new PageImpl<>(List.of(product));
 
         doNothing().when(repository).deleteById(existingId);
         doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
@@ -40,6 +57,19 @@ public class ProductServiceTests {
         when(repository.existsById(existingId)).thenReturn(true);
         when(repository.existsById(nonExistingId)).thenReturn(false);
         when(repository.existsById(dependentId)).thenReturn(true);
+
+        when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+        when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnPage() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<ProductDTO> result = service.findAllPaged(pageable);
+
+        Assertions.assertNotNull(result);
+        Mockito.verify(repository).findAll(pageable);
     }
 
     @Test
