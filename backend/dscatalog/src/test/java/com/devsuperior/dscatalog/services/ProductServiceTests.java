87727d42efd398b5a6d77b308f3b23848b232d22
Carlos Henrique <ichfs9108@gmail.com>
2023-09-09 00:55:39 -0300

    Import static

--- backend/dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -2,14 +2,16 @@ package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -30,23 +32,23 @@ public class ProductServiceTests {
         nonExistingId = 1000L;
         dependentId = 100L;
 
-        Mockito.doNothing().when(repository).deleteById(existingId);
+        doNothing().when(repository).deleteById(existingId);
     }
 
     @Test
     public void deleteShouldDoNothingWhenIdExists() {
-        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+        assertThrows(ResourceNotFoundException.class, () -> {
             service.delete(existingId);
         });
 
-        Mockito.when(repository.existsById(existingId)).thenReturn(true);
-        Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
-        Mockito.when(repository.existsById(dependentId)).thenReturn(true);
+        when(repository.existsById(existingId)).thenReturn(true);
+        when(repository.existsById(nonExistingId)).thenReturn(false);
+        when(repository.existsById(dependentId)).thenReturn(true);
     }
 
     @Test
     public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
-        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+        assertThrows(ResourceNotFoundException.class, () -> {
             service.delete(nonExistingId);
         });
     }
