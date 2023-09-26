392b7308c05a9cf1d4250c293df87650a5fa5f37
Carlos Henrique <ichfs9108@gmail.com>
2023-09-25 23:59:18 -0300

    UserDetailsService - SpringSecurity

--- backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Role.java
+++ backend/dscatalog/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -1,13 +1,13 @@
 package com.devsuperior.dscatalog.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
-import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_role")
-public class Role implements Serializable {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,6 +30,7 @@ public class Role implements Serializable {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
